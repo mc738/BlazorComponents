199495356ec3c8d3742dc556a7a25aff3bde395a
Max Clifford <max.clifford@johnoconner.co.uk>
2019-12-13 18:30:05 +0000

    Add project files.

---
+++ Modal.razor.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorComponents
+{
+    public class ModalBase : ComponentBase, IDisposable
+    {
+        [Inject] ModalService ModalService { get; set; }
+
+        protected bool IsVisible { get; set; }
+        protected string Title { get; set; }
+        protected RenderFragment Content { get; set; }
+
+        // public ModalBase()
+        // {
+        //     ModalService.OnShow += ShowModal;
+        //     ModalService.OnClose += CloseModal;
+        // }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            ModalService.OnShow += ShowModal;
+            ModalService.OnClose += CloseModal;
+        }
+
+
+        public void ShowModal(string title, RenderFragment content)
+        {
+            Title = title;
+            Content = content;
+            IsVisible = true;
+
+            StateHasChanged();
+        }
+
+        public void CloseModal()
+        {
+            IsVisible = false;
+            Title = "";
+            Content = null;
+
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            ModalService.OnShow -= ShowModal;
+            ModalService.OnClose -= CloseModal;
+        }
+    }
+}
